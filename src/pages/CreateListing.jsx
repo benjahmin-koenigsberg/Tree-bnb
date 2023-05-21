72a137b4c7c053de5f2fc31075093e52694a44f7
Benjahmin Koenigsberg <magnanimousplayer@gmail.com>
2023-05-21 09:01:27 -0400

    update image laoding function to firebase

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -4,6 +4,14 @@ import {getAuth, onAuthStateChanged} from 'firebase/auth'
 import {useNavigate} from 'react-router-dom'
 import Spinner from '../components/Spinner'
 import {toast} from 'react-toastify'
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
+import {db} from '../firebase.config'
+import {v4 as uuidv4} from 'uuid'
 
 
 
@@ -51,47 +59,104 @@ useEffect(()=>{
 
 
 
-const onSubmit = async (e) => {
-    e.preventDefault()
+ const onSubmit = async (e) => {
+   e.preventDefault();
 
-    if(discountedPrice >= regularPrice){
-        setLoading(true)
-        toast.error('Discounted price must be less than regular price')
-        return
-    }
-    if(images.length > 6){
-        setLoading(false)
-        toast.error('Max 6 images')
-        return
-    }
+   setLoading(true);
 
-    const geolocation = {}
-    let location;
+   if (discountedPrice >= regularPrice) {
+     setLoading(false);
+     toast.error("Discounted price needs to be less than regular price");
+     return;
+   }
 
-    if(geolocationEnabled){
-const response = await fetch(
-  `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GEOCODE_API_KEY}`);
-const data = response.json()
+   if (images.length > 6) {
+     setLoading(false);
+     toast.error("Max 6 images");
+     return;
+   }
 
-   geolocation.lat = data.results[0]?.geometry.location.lat ?? 0
-   geolocation.lng = data.results[0]?.geometry.location.lng ?? 0
+   let geolocation = {};
+   let location;
 
-location = data.status === 'ZERO_RESULTS' ? undefined : data.results[0]?.formatted_address
+   if (geolocationEnabled) {
+     const response = await fetch(
+       `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GEOCODE_API_KEY}`
+     );
 
+     const data = await response.json();
 
-if (location === undefined || location.includes('undefined')){
-    setLoading(false)
-    toast.error('Please enter a correct address')
-    return
-}
-    } else {
-       geolocation.lat = latitude
-       geolocation.lng = longitude
-       location = address
-    }
+     geolocation.lat = data.results[0]?.geometry.location.lat ?? 0;
+     geolocation.lng = data.results[0]?.geometry.location.lng ?? 0;
 
-    setLoading(false)
-}
+     location =
+       data.status === "ZERO_RESULTS"
+         ? undefined
+         : data.results[0]?.formatted_address;
+
+     console.log(data.results[0]?.formatted_address);
+
+     if (location === undefined || location.includes("undefined")) {
+       setLoading(false);
+       toast.error("Please enter a correct address");
+       return;
+     }
+   } else {
+     geolocation.lat = latitude;
+     geolocation.lng = longitude;
+   }
+
+   // Store image in firebase
+   const storeImage = async (image) => {
+     return new Promise((resolve, reject) => {
+       const storage = getStorage();
+       const fileName = `${auth.currentUser.uid}-${image.name}-${uuidv4()}`;
+
+       const storageRef = ref(storage, "images/" + fileName);
+
+       const uploadTask = uploadBytesResumable(storageRef, image);
+
+       uploadTask.on(
+         "state_changed",
+         (snapshot) => {
+           const progress =
+             (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+           console.log("Upload is " + progress + "% done");
+           switch (snapshot.state) {
+             case "paused":
+               console.log("Upload is paused");
+               break;
+             case "running":
+               console.log("Upload is running");
+               break;
+           }
+         },
+         (error) => {
+           reject(error);
+         },
+         () => {
+           // Handle successful uploads on complete
+           // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+           getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+             resolve(downloadURL);
+           });
+         }
+       );
+     });
+   };
+
+   const imgUrls = await Promise.all(
+     [...images].map((image) => storeImage(image))
+   ).catch(() => {
+     setLoading(false);
+     toast.error("Images not uploaded");
+     return;
+   });
+
+   console.log(imgUrls);
+
+   setLoading(false);
+ };
 
 
   const onMutate = (e) => {
